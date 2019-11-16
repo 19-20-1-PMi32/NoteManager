a0844e3aeae536832591bd3aebba30d8f74935e6
Victor Bozhuk <bozhook@gmail.com>
2019-11-16 23:45:13 +0200

    Update PageRegistration

--- NoteManager/PageRegistration.xaml.cs
+++ NoteManager/PageRegistration.xaml.cs
@@ -5,46 +5,19 @@ using System.Windows.Media;
 
 namespace NoteManager
 {
-    /// <summary>
-    /// Interaction logic for PageRegistration.xaml
-    /// </summary>
     public partial class PageRegistration : Page
     {
-        /// <summary>
-        /// Color of label when mouse is enter(Light Blue).
-        /// </summary>
-        private static Color colorEnter = Color.FromRgb(0, 255, 255);
-
-        /// <summary>
-        /// Color of label when mouse is leaved(Dark Blue).
-        /// </summary>
-        private static Color colorLeave = Color.FromRgb(13, 32, 149);
-
         public PageRegistration()
         {
             InitializeComponent();
         }
 
-        // колір прии наведенні курсору
-        private void Label_MouseEnterEvent(object sender, EventArgs e)
-        {
-            (sender as Label).Foreground = new SolidColorBrush(colorEnter);
-        }
-
-        // колір при покинанні курсора
-        private void Label_MouseLeaveEvent(object sender, EventArgs e)
-        {
-            (sender as Label).Foreground = new SolidColorBrush(colorLeave);
-        }
-
-        // повернутись назад на попередню сторінку
-        private void Label_MouseDownEvent(object sender, EventArgs e)
+        private void EventReturn(object sender, EventArgs e)
         {
             this.NavigationService.GoBack();
         }
 
-        // реєстрація
-        private void buttonClickRegistrstion(object sender, RoutedEventArgs e)
+        private void SignUpAndGoToNext(object sender, RoutedEventArgs e)
         {
             this.NavigationService.Navigate(new PageStartMenu());
         }
