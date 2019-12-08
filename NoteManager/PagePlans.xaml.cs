3c90c7271e82e67987a84cc13108d1b6f19b9c7f
lineyk27 <lineyk27gg@gmail.com>
2019-12-08 12:03:44 +0200

    Improved page with plans

--- NoteManager/PagePlans.xaml.cs
+++ NoteManager/PagePlans.xaml.cs
@@ -3,10 +3,11 @@ using System.Windows.Controls;
 using System.Collections.Generic;
 using System.Windows.Data;
 using System.Drawing;
+using System.Linq;
 using System.Globalization;
 using System.Text;
 using System.Diagnostics;
-
+using System.Windows;
 namespace NoteManager
 {
     /// <summary>
@@ -14,13 +15,20 @@ namespace NoteManager
     /// </summary>
     public partial class PagePlans : Page
     {
+        List<Plan> plans;
+        List<Plan> deleted;
+        List<Plan> found;
+        Plan currentEdit;
+        bool isNew = true;
         public PagePlans()
         {
             InitializeComponent();
-            //Initialization only for tests for tests
-            List<Plan> plans = GetTestPlans();
+            //Initialization only for tests 
+            deleted = new List<Plan>();
+            plans = GetTestPlans();
             ListPlans.ItemsSource = plans;
-
+            //currentEdit;
+            DataContext = currentEdit;
         }
         public List<Plan> GetTestPlans()
         {
@@ -29,26 +37,12 @@ namespace NoteManager
             List<Plan> result = new List<Plan>(count);
             for(int i =0; i < count; i++)
             {
-                PlanState a;
-                if (i % 3 == 0)
-                {
-                    a = PlanState.Complete;
-                }
-                else if(i % 2 == 0)
-                {
-                    a = PlanState.InProgres;
-                }
-                else
-                {
-                    a = PlanState.Crashed;
-                }
-
                 result.Add(new Plan()
                 {
+                    Name = "Some Name" + i.ToString(),
                     Text = $"План {i} і небагато тексту для тестування." + longText,
                     CreationTime = DateTime.Now.AddDays(i),
-                    DeadLineTime = DateTime.Now.AddDays(10 + i),
-                    State = a
+                    DeadLineTime = DateTime.Now.AddDays(10 + i)
                 }); ;
             }
             return result;
@@ -57,54 +51,114 @@ namespace NoteManager
         private void ListPlans_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             Plan sel = ((sender as ListBox)?.SelectedItem as Plan);
-            if (sel == null)
-                return;
-            LoadPlanView(sel);
-        }
-        private void LoadPlanView(Plan plan)
-        {
-            PlanEditor.Text = plan.Text;
-
-        }
-    }
-    public enum PlanState
-    {
-        InProgres = 1,
-        Complete = 2,
-        Crashed = 3
-    }
-
-    [ValueConversion(typeof(PlanState), typeof(string))]
-    public class StateColorConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            PlanState state = (PlanState) value;
-            if (PlanState.Complete == state)
-                return "#29a329";
-            else if (PlanState.Crashed == state)
-                return "#cc0000";
+            if (sel != null)
+            {
+                ChangeCurrent(sel);
+                isNew = false;
+            }
             else
-                return "#9900cc";
+                isNew = true;
         }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        private void ChangeCurrent(Plan plan)
         {
-            string color = value as string;
-            if (color == "Red")
-                return PlanState.Crashed;
-            else if (color == "Green")
-                return PlanState.Complete;
+            BeginInit();
+            currentEdit = plan;
+            DataContext = currentEdit;
+            EndInit();
+        }
+        private IEnumerable<Plan> SearchPlan(string text)
+        {
+            return plans.Where(x => x.Name.Contains(text) || x.Text.Contains(text));
+        }
+        private void UpdateList(string query)
+        {
+            found = SearchPlan(query).ToList();
+            ListPlans.BeginInit();
+            ListPlans.ItemsSource = found;
+            ListPlans.EndInit();
+        }
+        private void UpdateList()
+        {
+            ListPlans.BeginInit();
+            ListPlans.ItemsSource = plans;
+            ListPlans.EndInit();
+        }
+        private void SearchChanged(object sender, TextChangedEventArgs e)
+        {
+            if(SearchBox.Text != String.Empty)
+            {
+                UpdateList(SearchBox.Text);
+            }
             else
-                return PlanState.InProgres;
-            
+            {
+                UpdateList();
+            }
+        }
+        private Plan SelectedItem()
+        {
+            return ListPlans.SelectedItem as Plan;
+        }
+        private void Delete(object sender, RoutedEventArgs e)
+        {
+            var selected = SelectedItem();
+            if(selected != null)
+            {
+                deleted.Add(selected);
+                plans.Remove(selected);
+                UpdateList();
+                ChangeCurrent(null);
+                DeletePlan(selected);
+            }
+        }
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            var selected = SelectedItem();
+            if (isNew == true)
+            {
+                AddToList(currentEdit);
+                isNew = false;
+                SavePlan(currentEdit);
+                UpdateList();
+                ListPlans.SelectedItem = currentEdit;
+            }
+            if (selected != null)
+            {
+                UpdatePlan(selected);
+            }
+
+            UpdateList();
+        }
+        private void AddToList(Plan plan)
+        {
+            BeginInit();
+            plans.Add(plan);
+            EndInit();
+        }
+        private void New(object sender, RoutedEventArgs e)
+        {
+            isNew = true;
+            currentEdit = new Plan() { Text="", Name="" };
+            ChangeCurrent(currentEdit);
+            ListPlans.SelectedItem = null;
+        }
+        private void DeletePlan(Plan plan)
+        {
+            // here must be logic for delete plan from database
+        }
+        private void SavePlan(Plan plan)
+        {
+            // here must be logic for save plan to database
+        }
+        private void UpdatePlan(Plan plan)
+        {
+            // here must be logic for update plan to database
         }
     }
     public class Plan
     {
+        public string Name { get; set; }
         public string Text { get; set; }
         public DateTime CreationTime { get; set; }
         public DateTime DeadLineTime { get; set; }
-        public PlanState State { get; set; }
     }
 }
